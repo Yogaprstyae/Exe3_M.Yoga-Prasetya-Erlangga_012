d51771edad7c40dca74168c84f12a953254e961b
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2022-11-23 16:26:25 +0700

    add case 2

--- Exe3_M.Yoga Prasetya Erlangga_012/Program.cs
+++ Exe3_M.Yoga Prasetya Erlangga_012/Program.cs
@@ -168,6 +168,22 @@ namespace Exe3_M.Yoga_Prasetya_Erlangga_012
                                 obj.insertNode();
                             }
                             break;
+                        case '2':
+                            {
+                                if (obj.listEmpty())
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break;
+                                }
+                                Console.Write("Enter the roll number of the student" + "whose record is to be deleted: ");
+                                int StudentNo = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine();
+                                if (obj.deleteNode(StudentNo) == false)
+                                    Console.WriteLine("record not found");
+                                else
+                                    Console.WriteLine("Record with roll number" + StudentNo + "deleted\n");
+                            }
+                            break;
                     }
                 }
             }
